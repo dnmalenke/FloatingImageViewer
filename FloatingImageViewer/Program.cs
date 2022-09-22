313f2654cf96dcfc1aa9fea9256cd69f0a959314
David Malenke <dnmalenke@gmail.com>
2022-09-21 20:36:16 -0500

    add drop shadow

--- FloatingImageViewer/Program.cs
+++ FloatingImageViewer/Program.cs
@@ -153,8 +153,6 @@ namespace FloatingImageViewer
 
         protected override void WndProc(ref Message m)
         {
-            const int RESIZE_HANDLE_SIZE = 10;
-
             switch (m.Msg)
             {
                 case 0x0084/*NCHITTEST*/ :
@@ -164,29 +162,29 @@ namespace FloatingImageViewer
                     {
                         Point screenPoint = new Point(m.LParam.ToInt32());
                         Point clientPoint = PointToClient(screenPoint);
-                        if (clientPoint.Y <= RESIZE_HANDLE_SIZE)
+                        if (clientPoint.Y <= BORDER_SIZE)
                         {
-                            if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                            if (clientPoint.X <= BORDER_SIZE)
                                 m.Result = (IntPtr)13/*HTTOPLEFT*/ ;
-                            else if (clientPoint.X < (Size.Width - RESIZE_HANDLE_SIZE))
+                            else if (clientPoint.X < (Size.Width - BORDER_SIZE))
                                 m.Result = (IntPtr)12/*HTTOP*/ ;
                             else
                                 m.Result = (IntPtr)14/*HTTOPRIGHT*/ ;
                         }
-                        else if (clientPoint.Y <= (Size.Height - RESIZE_HANDLE_SIZE))
+                        else if (clientPoint.Y <= (Size.Height - BORDER_SIZE))
                         {
-                            if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                            if (clientPoint.X <= BORDER_SIZE)
                                 m.Result = (IntPtr)10/*HTLEFT*/ ;
-                            else if (clientPoint.X < (Size.Width - RESIZE_HANDLE_SIZE))
+                            else if (clientPoint.X < (Size.Width - BORDER_SIZE))
                                 m.Result = (IntPtr)2/*HTCAPTION*/ ;
                             else
                                 m.Result = (IntPtr)11/*HTRIGHT*/ ;
                         }
                         else
                         {
-                            if (clientPoint.X <= RESIZE_HANDLE_SIZE)
+                            if (clientPoint.X <= BORDER_SIZE)
                                 m.Result = (IntPtr)16/*HTBOTTOMLEFT*/ ;
-                            else if (clientPoint.X < (Size.Width - RESIZE_HANDLE_SIZE))
+                            else if (clientPoint.X < (Size.Width - BORDER_SIZE))
                                 m.Result = (IntPtr)15/*HTBOTTOM*/ ;
                             else
                                 m.Result = (IntPtr)17/*HTBOTTOMRIGHT*/ ;
@@ -231,7 +229,8 @@ namespace FloatingImageViewer
             get
             {
                 CreateParams cp = base.CreateParams;
-                cp.Style |= 0x20000; // <--- use 0x20000
+                cp.Style |= 0x20000;
+                cp.ClassStyle |= 0x20000; // drop shadow
                 return cp;
             }
         }
